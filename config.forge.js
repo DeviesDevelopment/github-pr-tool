970c8dff0d877a6cebee8526c9d9370428ddc3c3
Rickard Andersson <dunklas@gmail.com>
2020-12-09 15:16:12 +0100

    Copy artifact to more general path, and try build for linux

--- config.forge.js
+++ config.forge.js
@@ -1,25 +1,40 @@
+const fs = require('fs');
+
 module.exports = {
-    "packagerConfig": {},
-    "makers": [
+    packagerConfig: {},
+    makers: [
         {
-            "name": "@electron-forge/maker-squirrel",
-            "config": {
-                "name": "github_pr_todo"
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "github_pr_todo"
             }
         },
         {
-            "name": "@electron-forge/maker-zip",
-            "platforms": [
+            name: "@electron-forge/maker-zip",
+            platforms: [
                 "darwin"
             ]
         },
         {
-            "name": "@electron-forge/maker-deb",
-            "config": {}
+            name: "@electron-forge/maker-deb",
+            config: {}
         },
-        {
-            "name": "@electron-forge/maker-rpm",
-            "config": {}
+    ],
+    hooks: {
+        postMake: async (config, results) => {
+            results.forEach(result => {
+                const artifactPath = result.artifacts[0];
+                console.log(`Artifact for ${result.arch}/${result.platform} packaged to: ${artifactPath}`);
+                const pathParts = artifactPath.split(".");
+                const extension = pathParts[pathParts.length - 1];
+                const newPath = `out/github-pr-tool-${result.arch}-${result.platform}.${extension}`;
+                fs.copyFile(result.artifacts[0], newPath, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`Successfully wrote to: ${newPath}`);
+                })
+            })
         }
-    ]
+    }
 }
